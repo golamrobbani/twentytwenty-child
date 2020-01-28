0d54f5f13287367be91439b0dfc35c35c167ed55
golamrobbani <golamrobbani29@gmail.com>
2020-01-28 20:06:15 +0600

    remove gutenbarg disable

--- inc/extras.php
+++ inc/extras.php
@@ -1,9 +1,8 @@
 <?php
 // Disable Gutenberg Editor
-if (version_compare($GLOBALS['wp_version'], '5.0-beta', '>')) {
-// WP > 5 beta
+
+/*if (version_compare($GLOBALS['wp_version'], '5.0-beta', '>')) {
     add_filter('use_block_editor_for_post_type', '__return_false', 100);
 } else {
-// WP < 5 beta
     add_filter('gutenberg_can_edit_post_type', '__return_false');
-}
+}*/
